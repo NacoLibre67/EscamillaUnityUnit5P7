51242497f457187809e69df331247ec0d4490938
NachoLibre67 <736939@student.dusd.net>
2026-05-07 15:25:10 -0700

    fgnfgngfnfgn
    dftghtfdhb

--- UnityUnit5Escamilla/Assets/Course Library/Scripts the real/Game Manager.cs
+++ UnityUnit5Escamilla/Assets/Course Library/Scripts the real/Game Manager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq.Expressions;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -8,11 +9,17 @@ public class GameManager : MonoBehaviour
     public List<GameObject> targets;
     private float spawnRate = 1.0f;
     public GameObject[] targets2;
+    public TextMeshProUGUI scoreText;
+    public int score;
+    public TextMeshProUGUI gameOverText;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(0);
+        
     }
 
     // Update is called once per frame
@@ -24,8 +31,26 @@ public class GameManager : MonoBehaviour
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
+
+           
         }
     }
-
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+        if (!gameObject.CompareTag("Bad"))
+        {
+            GameManager.GameOver();
+        }
+    }
 }
 
--- UnityUnit5Escamilla/Assets/Course Library/Scripts the real/Target.cs
+++ UnityUnit5Escamilla/Assets/Course Library/Scripts the real/Target.cs
@@ -8,6 +8,9 @@ public class Target : MonoBehaviour
     private float maxTorque = 10;
     private float xRange = 6;
     private float ySpawnPos = -2;
+    private GameManager gameManager;
+    public int pointValue;
+    public ParticleSystem explosionParticle;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -17,6 +20,9 @@ public class Target : MonoBehaviour
         targetRb.AddForce(RandomForce(), ForceMode.Impulse);
         targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
         transform.position = RandomSpawnPos();
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+
+
     }
     void Update()
     {
@@ -26,6 +32,8 @@ public class Target : MonoBehaviour
     private void OnMouseDown()
     {
         Destroy(gameObject);
+        gameManager.UpdateScore(pointValue);
+        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
     }
     private void OnTriggerEnter(Collider other)
     {
